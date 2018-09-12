7383a14c00672750203a37adf17205b35f82b3b2
Khen Baniqued <32321380+thekhenzie@users.noreply.github.com>
2018-09-12 09:50:48 +0800

    Initial commit

---
+++ TesseractOcr/TesseractOcr.Android/MainActivity.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using TinyIoC;
+using XLabs.Platform.Device;
+using Tesseract;
+using Tesseract.Droid;
+using XLabs.Ioc;
+using XLabs.Ioc.TinyIOC;
+
+namespace TesseractOcr.Droid
+{
+    [Activity(Label = "TesseractOcr", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(bundle);
+
+            global::Xamarin.Forms.Forms.Init(this, bundle);
+
+            //grab reference to the container
+            var container = TinyIoCContainer.Current;
+
+            //call register method
+            container.Register<IDevice>(AndroidDevice.CurrentDevice);
+            container.Register<ITesseractApi>((cont, parameters) =>
+            {
+                return new TesseractApi(ApplicationContext, AssetsDeployment.OncePerVersion);
+            });
+
+            //register the container in xlabs resolver
+            Resolver.SetResolver(new TinyResolver(container));
+            LoadApplication(new App());
+        }
+    }
+}
+
---
+++ TesseractOcr/TesseractOcr/HomePage.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using Java.Lang;
+using Tesseract;
+using Xamarin.Forms;
+using XLabs.Ioc;
+using XLabs.Platform.Device;
+using XLabs.Platform.Services.Media;
+
+namespace TesseractOcr
+{
+    public class HomePage : ContentPage
+    {
+        private Button _takePictureButton;
+        private Button _refreshButton;
+        private Label _recognizedTextLabel;
+
+        private Image _takenImage;
+
+        private readonly ITesseractApi _tesseractApi;
+        private readonly IDevice _device;
+
+        public HomePage()
+        {
+            _tesseractApi = Resolver.Resolve<ITesseractApi>();
+            _device = Resolver.Resolve<IDevice>();
+
+            BuildUi();
+
+            WireEvents();
+        }
+
+        private void BuildUi()
+        {
+            _takePictureButton = new Button
+            {
+                Text = "New scan"
+            };
+            _refreshButton = new Button
+            {
+                Text = "Refresh"
+            };
+
+            _recognizedTextLabel = new Label(){ Text= "Please take a picture", TextColor = Color.Black};
+
+            _takenImage = new Image() { HeightRequest = 200 };
+
+            Content = new ScrollView
+            {
+                Content = new StackLayout
+                {
+                    Children =
+                    {
+                        _takePictureButton,
+                        _refreshButton,
+                        _takenImage,
+                        _recognizedTextLabel
+                    }
+                }
+            };
+
+        }
+
+        private void WireEvents()
+        {
+            _takePictureButton.Clicked += TakePictureButton_Clicked;
+            _refreshButton.Clicked += RefreshButton_Clicked;
+
+        }
+
+        void RefreshButton_Clicked(object sender, EventArgs e)
+        {
+            _recognizedTextLabel.Text = "Please take a picture";
+            _takenImage.Source = " ";
+        }
+        async void TakePictureButton_Clicked(object sender, EventArgs e)
+        {
+            if (!_tesseractApi.Initialized)
+                await _tesseractApi.Init("eng");
+
+            var photo = await TakePic();
+            if (photo != null)
+            {
+                var imageBytes = new byte[photo.Source.Length];
+                photo.Source.Position = 0;
+                photo.Source.Read(imageBytes, 0, (int)photo.Source.Length);
+                photo.Source.Position = 0;
+
+                _takenImage.Source = ImageSource.FromStream(() => photo.Source);
+
+                var tessResult = await _tesseractApi.SetImage(imageBytes);
+                //var path = $"android.resource://{PackageName}/Resource";
+                //var tessResult = await _tesseractApi.SetImage(App._file.Path);
+                if (tessResult)
+                {
+                    _recognizedTextLabel.Text = _tesseractApi.Text;
+                }
+                else
+                {
+                    _recognizedTextLabel.Text = "No Data";
+                }
+            }
+        }
+        // ...
+        //private byte[] ConvertYuvToJpeg(byte[] yuvData, CameraDevice camera)
+        //{
+        //    var cameraParameters = camera.;
+        //    var width = cameraParameters.PreviewSize.Width;
+        //    var height = cameraParameters.PreviewSize.Height;
+        //    var yuv = new YuvImage(yuvData, cameraParameters.PreviewFormat, width, height, null);
+        //    var ms = new MemoryStream();
+        //    var quality = 80;   // adjust this as needed
+        //    yuv.CompressToJpeg(new Rect(0, 0, width, height), quality, ms);
+        //    var jpegData = ms.ToArray();
+
+        //    return jpegData;
+        //}
+        private async Task<MediaFile> TakePic()
+        {
+            var mediaStorageOptions = new CameraMediaStorageOptions
+            {
+                DefaultCamera = CameraDevice.Rear
+            };
+            var mediaFile = await _device.MediaPicker.TakePhotoAsync(mediaStorageOptions);
+
+            _recognizedTextLabel.Text = "Loading..";
+            return mediaFile;
+        }
+    }
+}
